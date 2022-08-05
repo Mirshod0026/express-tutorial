9d89dd3b6e8f212cd7477c5ee22aec23a51f71f1
Mirshodmastonovtem <mirshodkumush2020@gmail.com>
2022-08-05 19:43:12 +0500

    men books uchun apilar chiqardim

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello Node.js');
 });
 
+const books = []
+
 // GET /users
 
 // POST /users
 
 // GET /books
 
+app.get('/books',(req,res) => {
+  res.send(books)
+})
+
 // POST /books
+
+app.post('/books',(req,res) =>{
+  res.send('create book logic')
+})
 
 app.listen(3000, () => console.log('server run on port:3000'));
