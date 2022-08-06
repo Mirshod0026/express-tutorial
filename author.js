cfe8ccb943b2e67645501f9897e325e1c4d8239e
Mirshodmastonovtem <mirshodkumush2020@gmail.com>
2022-08-06 07:33:58 +0500

    authorSchemni tuzib qoydim

---
+++ author.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+
+const authorSchem = {
+    name: String,
+    books: [
+        {
+            type: mongoose.Types.ObjectId,
+// bu yerdagi ref ga mongoose.model ichidagi bookning nomi yozilishi kerak
+            ref: ''
+        }
+    ]
+}
+
+const authorModel = mongoose.model('Author', authorSchem);
+
+async function createAuthor(name) {
+    const author = new authorModel({
+        name ,
+        books ,
+    })
+
+    await author.save();
+}
+
+async function getAuthor() {
+    const author = await authorModel.find();
+
+    // console.log(author);
+}
+
+async function getAuthorById(id) {
+    const author = await authorModel.findById(id)
+
+    // console.log(author);
+
+}
+
+createAuthor('Mirshod');
